ec91c4ad0710d51a21f619b3ce6683b9bc4f2c01
kubo-nanami <kubo.nanami.720@s.kyushu-u.ac.jp>
2020-02-07 14:39:43 +0900

    new file:   mysite/polls/migrations/__pycache__/__init__.cpython-38.pyc

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='polls/index.html'), name='index'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.TemplateView):
+    template_name = 'polls/index.html'
