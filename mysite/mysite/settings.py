f0d3af13043ba08824c59119e1ccd4d48f4f7b32
kubo-nanami <kubo.nanami.720@s.kyushu-u.ac.jp>
2020-03-24 17:33:37 +0900

    modified:   mysite/accounts/__pycache__/__init__.cpython-38.pyc

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)#追加
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]#追加
 
 SITE_ID = 1
 
