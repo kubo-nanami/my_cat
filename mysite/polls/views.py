55673564ebd4ce3ed35970d6873b9447afe41686
kubo-nanami <kubo.nanami.720@s.kyushu-u.ac.jp>
2020-03-28 18:29:03 +0900

    new file:   mysite/IMG_2817.JPG

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -75,7 +75,7 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
 
     def form_valid(self, form):
         messages.success(self.request, '日記を更新しました。')
-        return super().form_invalid(form)
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "日記の更新に失敗しました。")
